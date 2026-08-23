188f1a012d39b356725e400d113df46eb1624448
Taha ahmed <tahaahmed3428@gmail.com>
2026-08-23 20:41:36 +0300

    fix(sales): align fluent validation with MediatR CreateSalesInvoiceCommand

--- NexusErp.Application/Identity/Commands/SalesInvoiceRequestValidator/CreateSalesInvoiceRequestValidator.cs
+++ NexusErp.Application/Identity/Commands/SalesInvoiceRequestValidator/CreateSalesInvoiceRequestValidator.cs
@@ -1,18 +1,25 @@
 ﻿using FluentValidation;
-using NexusErp.Application.Procurement.Commands;
-using System;
-using System.Collections.Generic;
-using System.Text;
+using NexusErp.Application.Sales.Commands;
 
-namespace NexusErp.Application.Identity.Commands.SalesInvoiceRequestValidator
+namespace NexusErp.Application.Sales.Validators;
+
+public class CreateSalesInvoiceCommandValidator : AbstractValidator<CreateSalesInvoiceCommand>
 {
-    public class CreateSalesInvoiceRequestValidator : AbstractValidator<CreateSalesInvoiceRequest>
+    public CreateSalesInvoiceCommandValidator()
     {
-        public CreateSalesInvoiceRequestValidator()
-        {
-            RuleFor(x => x.BranchId).NotEmpty();
-            RuleFor(x => x.Items).NotEmpty().WithMessage("add one product at least!");
-            RuleFor(x => x.DiscountAmount).GreaterThanOrEqualTo(0);
-        }
+        RuleFor(x => x.BranchId)
+            .NotEmpty().WithMessage("Branch ID is required.");
+
+        RuleFor(x => x.CreateByUserId)
+            .NotEmpty().WithMessage("User ID is required.");
+
+        RuleFor(x => x.Items)
+            .NotEmpty().WithMessage("Invoice cannot be created without items.");
+
+        RuleFor(x => x.Payments)
+            .NotEmpty().WithMessage("At least one payment method must be provided.");
+
+        RuleFor(x => x.DiscountAmount)
+            .GreaterThanOrEqualTo(0).WithMessage("Discount amount cannot be negative.");
     }
 }
